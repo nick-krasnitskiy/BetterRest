bb91b2382f9b925b4673344f09cc5cc96a963c81
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-10-24 14:14:22 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nick Krasnitskiy on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
